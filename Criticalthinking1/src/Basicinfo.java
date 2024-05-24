bc9a5c56455a2501980cd2f47c416c73b5e25fef
rupy95 <36399744+rupy95@users.noreply.github.com>
2024-05-24 10:41:38 -0700

    Add files via upload

---
+++ Criticalthinking1/src/Basicinfo.java
@@ -0,0 +1,22 @@
+
+public class Basicinfo {
+
+	public static void main(String[] args) {
+		String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Elm Street";
+        String city = "Springfield";
+        String zipCode = "12345";
+        
+        // Print each variable on a new line
+        System.out.println(firstName);       
+        System.out.println(lastName);        
+        System.out.println(streetAddress);   
+        System.out.println(city);            
+        System.out.println(zipCode);         
+
+
+	}
+
+}
+ 
